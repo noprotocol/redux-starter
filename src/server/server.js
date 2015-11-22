406d47da6c72963f2fd3414ec02d16987bd56d04
Stef van den Ham <stef@mindthecode.com>
2015-11-22 13:52:08 +0100

    HTML5 Bp

--- src/server/server.js
+++ src/server/server.js
@@ -19,9 +19,11 @@ const app = express();
 const renderFullPage = (html, initialState) => {
   return `
     <!doctype html>
-    <html>
+    <html class='no-js' lang=''>
       <head>
         <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Isomorphic Redux Example</title>
         <link rel="stylesheet" type="text/css" href="/static/app.css">
       </head>
@@ -31,16 +33,25 @@ const renderFullPage = (html, initialState) => {
           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
         </script>
         <script src="/static/bundle.js"></script>
+
+        <script>
+          (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+          function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+          e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+          e.src='https://www.google-analytics.com/analytics.js';
+          r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+          ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+        </script>
       </body>
     </html>
   `;
 }
 
-if(process.env.NODE_ENV !== 'production'){
+if(process.env.NODE_ENV !== 'production') {
   const compiler = webpack(webpackConfig);
   app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: webpackConfig.output.publicPath }));
   app.use(webpackHotMiddleware(compiler));
-}else{
+} else {
   app.use('/static', express.static(__dirname + '/../../dist'));
 }
 
@@ -48,39 +59,40 @@ app.get('/*', function (req, res) {
 
   const location = createLocation(req.url);
 
-    match({ routes, location }, (err, redirectLocation, renderProps) => {
+  match({ routes, location }, (err, redirectLocation, renderProps) => {
 
-      if(err) {
-        console.error(err);
-        return res.status(500).end('Internal server error');
+  if(err) {
+    console.error(err);
+    return res.status(500).end('Internal server error');
+  }
+
+  if(!renderProps) {
+    return res.status(404).end('Not found');
+  }
+  
+  // const store = configureStore({user : user, version : packagejson.version});
+  const store = configureStore({version : packagejson.version});
+
+  const InitialView = (
+    <Provider store={store}>
+      {() =>
+        <RoutingContext {...renderProps} />
       }
+    </Provider>
+  );
 
-      if(!renderProps)
-        return res.status(404).end('Not found');
-
-      // const store = configureStore({user : user, version : packagejson.version});
-      const store = configureStore({version : packagejson.version});
-
-      const InitialView = (
-        <Provider store={store}>
-          {() =>
-            <RoutingContext {...renderProps} />
-          }
-        </Provider>
-      );
-
-      //This method waits for all render component promises to resolve before returning to browser
-      fetchComponentDataBeforeRender(store.dispatch, renderProps.components, renderProps.params)
-        .then(html => {
-          const componentHTML = React.renderToString(InitialView);
-          const initialState = store.getState();
-          res.status(200).end(renderFullPage(componentHTML,initialState))
-        })
-        .catch(err => {
-          console.log(err)
-          res.end(renderFullPage("",{}))
-        });
+  // This method waits for all render component promises to resolve before returning to browser
+  fetchComponentDataBeforeRender(store.dispatch, renderProps.components, renderProps.params)
+    .then(html => {
+      const componentHTML = React.renderToString(InitialView);
+      const initialState = store.getState();
+      res.status(200).end(renderFullPage(componentHTML,initialState))
+    })
+    .catch(err => {
+      console.log(err)
+      res.end(renderFullPage("",{}))
     });
+  });
 
 });
 
