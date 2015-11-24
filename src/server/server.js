0de9122b3572a18715bae454a1fb1f8952fea3ee
Stef van den Ham <stef@mindthecode.com>
2015-11-24 10:02:27 +0100

    Dynamic port

--- src/server/server.js
+++ src/server/server.js
@@ -95,7 +95,7 @@ app.get('/*', function (req, res) {
 
 });
 
-const server = app.listen(3000, function () {
+const server = app.listen(process.env.PORT || 3000, function () {
   const host = server.address().address;
   const port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
