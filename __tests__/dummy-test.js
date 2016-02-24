8b6e27a9767fb7d187afc998abca5c6778059766
Stef van den Ham <stef@mindthecode.com>
2016-02-24 13:34:56 +0100

    Annotated test

--- __tests__/dummy-test.js
+++ __tests__/dummy-test.js
@@ -1,6 +1,9 @@
+// Dummy test for Jest (see npm test)
+
 describe('sum', function() {
   it('adds 1 + 2 to equal 3', function() {
     var sum = function(a, b) { return a + b; };
     expect(sum(1, 2)).toBe(3);
   });
 });
+
