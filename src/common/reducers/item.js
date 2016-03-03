d2a4cc59266a4f1a2f21d12e1b3a8c649c248ee4
Stef van den Ham <stef@mindthecode.com>
2016-03-03 17:19:28 +0100

    Cleaned up item reducer

--- src/common/reducers/item.js
+++ src/common/reducers/item.js
@@ -6,18 +6,18 @@ export default function items(state = {
   items: []
 }, action) {
   switch(action.type) {
-  case ITEMS_GET_REQUEST:
-    return Object.assign({}, state, {
-    });
-  case ITEMS_GET_SUCCESS:
-    return Object.assign({}, state, {
-      items: action.req.data.items
-    });
-  case ITEMS_GET_FAILURE:
-    return Object.assign({}, state, {
-    });
-  default:
-    return state;
+    case ITEMS_GET_REQUEST:
+      return Object.assign({}, state, {
+      });
+    case ITEMS_GET_SUCCESS:
+      return Object.assign({}, state, {
+        items: action.req.data.items
+      });
+    case ITEMS_GET_FAILURE:
+      return Object.assign({}, state, {
+      });
+    default:
+      return state;
   }
 };
 
